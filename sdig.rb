4d9c0d4df6b447c3de5037990ccf39aebd9d0afe
Ju <yju@sel-mpw7s.seoul.corp.akamai.com>
2014-12-29 13:49:18 +0900

    now you can add minus (-) as 2nd arg. it will remove the hosts file entry.

--- sdig.rb
+++ sdig.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-unless ARGV.length == 1
+unless (ARGV.length == 2 and ARGV[1] == '-') or ARGV.length == 1
   puts "\n"
   puts "Usage:"
   puts "   sdig www.domain.com"
@@ -13,41 +13,50 @@ def add_ip_to_host(ipaddr, domain)
   if arr_ip.length > 1
     ipaddr = arr_ip.first
   end
-  cmd = "sudo sh -c 'echo #{ipaddr.strip} #{domain.strip} >> /etc/hosts'"
+  cmd = "sudo sh -c 'echo #{ipaddr.strip} #{domain.strip} >> /etc/hosts; killall -HUP mDNSResponder'"
   if system(cmd)
     puts "Succesfully added"
   else
-    puts "Oops something happend, see if it was added by yourself!"
+    puts "Oops something happend, see if it was added yourself!"
   end
 end
 
 domain = ARGV[0]
-staging_ip = "NoIPwasFound"
-arr_result = %x(dig #{domain} +short).split("\n")
-arr_result.each do |each|
-  if each =~ /.*akamaiedge\.net\./ or each =~ /.*akamai\.net\./
-    staging_domain = each.to_s.gsub(".net.", "-staging.net.")
-    staging_ip = %x(dig #{staging_domain} +short)
-    puts "Staging IPs:"
-    puts staging_ip
-  end
-end
-
-if not staging_ip == "NoIPwasFound"
-  while true
-    puts "Would you like to add the IP to the host file?(y/n)"
-    ARGV.clear
-    answer = gets.chomp
-    case answer
-      when "y"
-        add_ip_to_host(staging_ip, domain)
-        break
-      when "n"
-        puts "Bye!"
-        exit
+if ARGV.length == 1 #add ip
+  staging_ip = "NoIPwasFound"
+  arr_result = %x(dig #{domain} +short).split("\n")
+  arr_result.each do |each|
+    if each =~ /.*akamaiedge\.net\./ or each =~ /.*akamai\.net\./
+      staging_domain = each.to_s.gsub(".net.", "-staging.net.")
+      staging_ip = %x(dig #{staging_domain} +short)
+      puts "Staging IPs:"
+      puts staging_ip
     end
   end
-elsif staging_ip =="NoIPwasFound"
- puts "The domain is not on Akamai"
- puts %x(dig #{domain})
+
+  if not staging_ip == "NoIPwasFound"
+    while true
+      puts "Would you like to add the IP to the host file?(y/n)"
+      ARGV.clear
+      answer = gets.chomp
+      case answer
+        when "y"
+          add_ip_to_host(staging_ip, domain)
+          break
+        when "n"
+          puts "Bye!"
+          exit
+      end
+    end
+  elsif staging_ip =="NoIPwasFound"
+   puts "It seems the domain is not on Akamai"
+   puts %x(dig #{domain})
+  end
+else #remove entry
+  cmd = %Q[sudo sh -c 'sed -i "" "/.*#{domain}/d" /etc/hosts; killall -HUP mDNSResponder']
+  if system(cmd)
+    puts "Entry removed"
+  else
+    puts "Shell command failed - please verify"
+  end
 end
