b1b2f288fb0d0993d50c1a8797aab1a530d827bb
ricky840 <ricky840@gmail.com>
2016-11-01 14:19:45 +0900

    test

---
+++ sdig_formula.rb
@@ -0,0 +1,12 @@
+class Sdig < Formula
+  desc "test"
+  homepage "fuck.com"
+  url "https://raw.githubusercontent.com/ricky840/sdig/master/sdig.rb"
+  sha256 ""
+
+  def install
+  end
+
+  test do
+  end
+end
