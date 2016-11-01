81f5f1d21a8b1fd71455a6a91874b823797e582f
ricky840 <ricky840@gmail.com>
2016-11-01 14:31:35 +0900

    version

---
+++ sdig_formula5.rb
@@ -0,0 +1,13 @@
+class SdigFormula5 < Formula
+  desc "test"
+  homepage "fuck.com"
+  url "https://raw.githubusercontent.com/ricky840/sdig/master/sdig.rb"
+  sha256 ""
+  version "0.1"
+
+  def install
+  end
+
+  test do
+  end
+end
