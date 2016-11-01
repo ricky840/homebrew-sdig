3358c168708574d3bf3e977566241b77125e5b80
ricky840 <ricky840@gmail.com>
2016-11-01 14:33:17 +0900

    tdfas

--- sdig_formula5.rb
+++ sdig_formula5.rb
@@ -2,7 +2,7 @@ class SdigFormula5 < Formula
   desc "test"
   homepage "fuck.com"
   url "https://raw.githubusercontent.com/ricky840/sdig/master/sdig.rb"
-  sha256 ""
+  sha256 "5a4f0d49d64b688fd68b345d643b9ddb64b78dae0fd4a9e3f7e639b8705c33ca"
   version "0.1"
 
   def install
