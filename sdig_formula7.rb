2ce0aa06e8f509116c2f02dac231e10be60f6643
ricky840 <ricky840@gmail.com>
2016-11-01 16:38:39 +0900

    8

---
+++ sdig_formula7.rb
@@ -0,0 +1,16 @@
+class SdigFormula7 < Formula
+  desc "test"
+  homepage "fuck.com"
+  url "https://raw.githubusercontent.com/ricky840/sdig/master/sdig.rb"
+  sha256 "5a4f0d49d64b688fd68b345d643b9ddb64b78dae0fd4a9e3f7e639b8705c33ca"
+  version "0.1"
+
+  def install
+    system "ls -al"
+    bin.install "sdig_formula7.rb"
+  end
+
+  test do
+    # system "#{bin}/sdig"
+  end
+end
