9a54183a40bbef527e2e215d566f8618074b71db
ricky840 <ricky840@gmail.com>
2016-11-02 18:42:51 +0900

    .3 nothing but test

--- akasdig.rb
+++ akasdig.rb
@@ -3,7 +3,7 @@ class Akasdig < Formula
   homepage "www.akamai.com"
   url "https://raw.githubusercontent.com/ricky840/sdig/master/sdig"
   sha256 "014c1a86c4e9e7a2c188174a009d21bdde4d95f1ba15b1dae8dbdc75bd536fa0"
-  version "0.1.2"
+  version "0.1.3"
 
   def install
     bin.install "sdig"
