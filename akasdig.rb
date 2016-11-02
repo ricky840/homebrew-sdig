aa6b9d804add4ea01657f213f1bb3741a1769702
ricky840 <ricky840@gmail.com>
2016-11-02 18:38:09 +0900

    akasdig

--- akasdig.rb
+++ akasdig.rb
@@ -1,6 +1,6 @@
-class AkamaiGetStagingIp < Formula
-  desc "A little script does DNS lookup to get Akamai staging IP address."
-  homepage "www.dontask.com"
+class Akasdig < Formula
+  desc "A little script does DNS lookup to get Akamai staging network IP address."
+  homepage "www.akamai.com"
   url "https://raw.githubusercontent.com/ricky840/sdig/master/sdig"
   sha256 "014c1a86c4e9e7a2c188174a009d21bdde4d95f1ba15b1dae8dbdc75bd536fa0"
   version "0.1.2"
