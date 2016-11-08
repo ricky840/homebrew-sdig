6d9b75908c3b7920fc59ab9e73e6d768fb4bf599
ricky840 <ricky840@gmail.com>
2016-11-08 19:31:54 +0900

    test +1

--- akasdig.rb
+++ akasdig.rb
@@ -2,8 +2,8 @@ class Akasdig < Formula
   desc "A little script does DNS lookup to get Akamai staging network IP address."
   homepage "www.akamai.com"
   url "https://raw.githubusercontent.com/ricky840/sdig/master/sdig"
-  sha256 "4ca01e00179f8fa659a606d2675dd2912309d60cac0da4f7dfe1628adec86296"
-  version "0.1.2"
+  sha256 "71c2fc2787d47e6a5407ea763b0386273494e1214b8c062c121380ca1020bfb5"
+  version "0.1.3"
 
   def install
     bin.install "sdig"
