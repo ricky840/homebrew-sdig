9c9f9c67d7630953aa93c1000bb53c1b86f027f6
ricky840 <ricky840@gmail.com>
2016-12-29 00:38:48 +0900

    akasdig modified

--- akasdig.rb
+++ akasdig.rb
@@ -1,9 +1,17 @@
 class Akasdig < Formula
-  desc "A little script does DNS lookup to get Akamai staging network IP address."
+  desc "A dirty little script does DNS lookup to get Akamai staging network IP address and little more."
   homepage "www.akamai.com"
   url "https://raw.githubusercontent.com/ricky840/homebrew-akasdig/master/sdig"
-  sha256 "71c2fc2787d47e6a5407ea763b0386273494e1214b8c062c121380ca1020bfb5"
-  version "0.1.3"
+  sha256 "5298c225170f3aaaa9b60298dc48296e45fe5bc530cc7df4cd3427fad2f71239"
+  version "0.1.4"
+
+  depends_on "optparse"
+  depends_on "json"
+  depends_on "net/http"
+  depends_on "terminal-table"
+  depends_on "ipaddr"
+  depends_on "ruby-progressbar"
+  depends_on "thread/pool"
 
   def install
     bin.install "sdig"
