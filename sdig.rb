989c83b4419181564ccb3492d1545100f2b03ecd
Jaejun Yu <ricky840@gmail.com>
2021-11-02 22:57:07 +0800

    url update

--- sdig.rb
+++ sdig.rb
@@ -1,7 +1,7 @@
 class Sdig < Formula
   desc "A little script does DNS lookup to get Akamai staging network IP address."
   homepage "https://origin-sfo.datswatsup.com"
-  url "https://github.com/ricky840/sdig/blob/master/sdig.rb"
+  url "https://raw.githubusercontent.com/ricky840/sdig/master/sdig.rb"
   version "1.0.0"
   sha256 "9bfd5e810f97582201d1a3c0f03a681a0bc1fb1ea21c281708a7e06d6480f4eb"
   license "MIT"
