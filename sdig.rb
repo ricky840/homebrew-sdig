c97068716ffafd7764eef34ee2549c84d1011b2c
Jaejun Yu <ricky840@gmail.com>
2021-11-02 23:05:11 +0800

    update sha256

--- sdig.rb
+++ sdig.rb
@@ -1,14 +1,14 @@
 class Sdig < Formula
   desc "A little script does DNS lookup to get Akamai staging network IP address."
   homepage "https://origin-sfo.datswatsup.com"
-  url "https://raw.githubusercontent.com/ricky840/sdig/master/sdig.rb"
+  url "https://raw.githubusercontent.com/ricky840/sdig-source/master/sdig_source.rb"
   version "1.0.0"
   sha256 "9bfd5e810f97582201d1a3c0f03a681a0bc1fb1ea21c281708a7e06d6480f4eb"
   license "MIT"
 
   def install
     ohai "Installing.."
-    bin.install "sdig.rb" => "sdig"
+    bin.install "sdig_source.rb" => "sdig"
   end
 
   test do
